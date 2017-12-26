72d291338cbb3704603246e5e8ee5a1407ea632d
Bryan Milstead <willbmisled@gmail.com>
2017-12-26 16:37:31 -0500

    'irrelevant tweak'

--- R/create_pkg_data.R
+++ R/create_pkg_data.R
@@ -1,27 +1,27 @@
-#create_pkg_data
-#select a lake and create a buffer around it
-#NLA06608-3890 #beach pond
-#NLA06608-2162 #yawgoo pond
-lake<-sf::st_read('C:/bryan/rscripts/lakescape/inst/extdata/nla_lakemorphometry.shp')
-lake<-sf::st_as_sf(dplyr::filter(lake, nlaSITE_ID=='NLA06608-3890'))
-lake<-sf::st_buffer(lake, 3000)
-
-# select a border lake from Jeff's lake morpho and add a buffer
-lake1<-sf::st_read(dsn='Y:/data/lakeMorphometry/LakeMorphGdb.gdb', layer = "NorthEast01") 
-lake1<-sf::st_as_sf(dplyr::filter(lake1, COMID==166196269))
-lake1<-sf::st_buffer(lake1, 3000)
-
-#select a lake from the 2007 NLA shapefile-Texas is nice
-lake2<-sf::st_read('L:/Public/Milstead_Lakes/NLA_2007/GIS/gis_final/National_LakePoly.shp')
-lake2<-sf::st_as_sf(dplyr::filter(lake2, SITEID=='NLA06608-TX:14'))
-
-#compress and save data to /data
-devtools::use_data(lake, compress = "bzip2", overwrite = TRUE)
-devtools::use_data(lake1, compress = "bzip2", overwrite = TRUE)
-devtools::use_data(lake2, compress = "bzip2", overwrite = TRUE)
-
-#  polygon out of range (in canada)
-canada<-sf::st_read('C:/bryan/bryan_temp/tempPoly.kml')
+# #create_pkg_data
+# #select a lake and create a buffer around it
+# #NLA06608-3890 #beach pond
+# #NLA06608-2162 #yawgoo pond
+# lake<-sf::st_read('C:/bryan/rscripts/lakescape/inst/extdata/nla_lakemorphometry.shp')
+# lake<-sf::st_as_sf(dplyr::filter(lake, nlaSITE_ID=='NLA06608-3890'))
+# lake<-sf::st_buffer(lake, 3000)
+# 
+# # select a border lake from Jeff's lake morpho and add a buffer
+# lake1<-sf::st_read(dsn='Y:/data/lakeMorphometry/LakeMorphGdb.gdb', layer = "NorthEast01") 
+# lake1<-sf::st_as_sf(dplyr::filter(lake1, COMID==166196269))
+# lake1<-sf::st_buffer(lake1, 3000)
+# 
+# #select a lake from the 2007 NLA shapefile-Texas is nice
+# lake2<-sf::st_read('L:/Public/Milstead_Lakes/NLA_2007/GIS/gis_final/National_LakePoly.shp')
+# lake2<-sf::st_as_sf(dplyr::filter(lake2, SITEID=='NLA06608-TX:14'))
+# 
+# #compress and save data to /data
+# devtools::use_data(lake, compress = "bzip2", overwrite = TRUE)
+# devtools::use_data(lake1, compress = "bzip2", overwrite = TRUE)
+# devtools::use_data(lake2, compress = "bzip2", overwrite = TRUE)
+# 
+# #  polygon out of range (in canada)
+# canada<-sf::st_read('C:/bryan/bryan_temp/tempPoly.kml')
 
 
 
